5f03361382c391b1ed8ef008831203e246e6d940
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-25 15:36:17 +0900

    1단계 ì  완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,13 +6,22 @@ import java.util.Scanner;
 class App {
     void run() {
         System.out.println("==명언 앱==");
-        System.out.print("명령 ) ");
 
-        Scanner scanner = new Scanner(System.in); // 표준  입력, 키보드
-        String cmd = scanner.nextLine();
+        while (true) { // 반복문, 참이면 실행
+            System.out.print("명령 ) ");
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
-        // 위 라인은 아래 라인과 같은 뜻
-//        System.out.println("입력하신 명령 : "+cmd+"\n");
+            // Scanner 외워라
+            Scanner scanner = new Scanner(System.in); // 표준  입력, 키보드
+            String cmd = scanner.nextLine(); // 고객이 입력하고 엔터 누를 때까지 정지
+
+            if (cmd.equals("종료")) { // 문장 비교는 equals()
+                break; // 나를 감싼 반복문 종료
+//                continue; // 한턴만 쉰다.
+            }
+
+            System.out.printf("입력하신 명령어 : %s\n", cmd);
+            // 위 라인은 아래 라인과 같은 뜻
+//        System.out.println("입력하신 명령어 : "+cmd+"\n");
+        }
     }
 }
