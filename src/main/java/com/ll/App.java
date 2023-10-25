12044b2799bb50c693bcd5ba504cf9db90498807
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-25 14:41:08 +0900

    스캐너 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,18 @@
 package com.ll;
 
+import java.util.Scanner;
+
 //프로그램 중심
 class App {
     void run() {
-        System.out.println("프로그램 실행");
+        System.out.println("==명언 앱==");
+        System.out.print("명령 ) ");
+
+        Scanner scanner = new Scanner(System.in); // 표준  입력, 키보드
+        String cmd = scanner.nextLine();
+
+        System.out.printf("입력하신 명령 : %s\n", cmd);
+        // 위 라인은 아래 라인과 같은 뜻
+//        System.out.println("입력하신 명령 : "+cmd+"\n");
     }
 }
