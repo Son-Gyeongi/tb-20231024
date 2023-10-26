614776e551bae0e5fe832b99b83c802a94106f8a
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-26 11:50:23 +0900

    step 5 Qqotation s.add()

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //프로그램 중심
@@ -8,6 +10,9 @@ class App {
         System.out.println("==명언 앱==");
 
         int lastQuotationId = 0; // 명언 번호
+
+//        Quotation[] quotations = new Quotation[100]; // 명언 리모컨을 100개 담을 수 있는 배열 객체 1개, 고정크기
+        List<Quotation> quotations = new ArrayList<>(); // 가변크기 리스트
 
         while (true) { // 반복문, 참이면 실행
             System.out.print("명령 ) ");
@@ -31,8 +36,11 @@ class App {
 
                 // Qutotation 생성자로 초기화하고 객체 만들기
                 Quotation quotation = new Quotation(lastQuotationId, content, authorName);
+                quotations.add(quotation);
 
                 System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
+            } else if (cmd.equals("목록")) {
+                System.out.println("총 개수 : " + quotations.size());
             }
         }
     }
